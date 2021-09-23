5a779ee59df2f235acdb3f5470d6c9cb3c2c68ed
Chirath <chirathcb@gmail.com>
2021-09-24 01:32:11 +0530

    Visitor
    Model done

---
+++ HospitalInformationManagementSystem/Model/VisitorModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalInformationManagementSystem.Model
+{
+    class VisitorModel
+    {
+        public int user_id { get; set; }
+        public string visitor { get; set; }
+        public int purpose { get; set; }
+        public string name { get; set; }
+        public int phone_no { get; set; }
+        public string nic_no { get; set; }
+        public DateTime date { get; set; }
+        public DateTime in_time { get; set; }
+        public DateTime out_time { get; set; }
+        public String note { get; set; }
+        public String attachment_type { get; set; }
+        public DateTime attachment_data { get; set; }
+    }
+}
