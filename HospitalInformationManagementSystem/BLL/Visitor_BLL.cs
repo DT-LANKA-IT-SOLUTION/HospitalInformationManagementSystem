969335e47babe90cc647ab6575c02ab26c050ed3
Chirath <chirathcb@gmail.com>
2021-09-25 23:46:09 +0530

    Create Classes
    Create classes fro visitor details

--- HospitalInformationManagementSystem/BLL/Visitor_BLL.cs
+++ HospitalInformationManagementSystem/BLL/Visitor_BLL.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using HospitalInformationManagementSystem.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace HospitalInformationManagementSystem.BLL
 {
     class Visitor_BLL
     {
+        public void LoadAllVisitorGridView(DataGridView dtgVisitor)
+        {
+            try
+            {
+                dtgVisitor.AutoGenerateColumns = false;
+                dtgVisitor.DataSource = Visitor_DLL.GetAllVisitor();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        
     }
 }
